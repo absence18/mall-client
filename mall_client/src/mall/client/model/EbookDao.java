15b5c6bd07ccafb8b61f4715a2071fad5244ecc9
GDJ34 <GDJ34@DESKTOP-742N3R3>
2021-04-13 14:35:26 +0900

    메인메뉴 include, client 로그인/로그아웃 구현

--- mall_client/src/mall/client/model/EbookDao.java
+++ mall_client/src/mall/client/model/EbookDao.java
@@ -1,6 +1,8 @@
 package mall.client.model;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,41 +10,40 @@ import mall.client.commons.DBUtil;
 import mall.client.vo.Ebook;
 
 public class EbookDao {
-	
-	private DBUtil dbutil;
-	public List<Ebook> selectEbookListByPage(int beginRow, int rowPerPage)	{
+	private DBUtil dbUtil;
+	public List<Ebook> selectEbookListByPage(int beginRow, int rowPerPage) {
 		
 		List<Ebook> list = new ArrayList<>();
-		
-		this.dbutil = new DBUtil();
+		this.dbUtil = new DBUtil();
 		Connection conn = null;
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
 		
 		try {
-			conn = this.dbutil.getConnection();
-			String sql = "SELECT ebook_title ebookTitle, ebook_Price ebookPrice, ebook_img ebook_Img FROM ebook ORDER BY ebook_date DESC LIMIT ?, ?";
-			stmt = conn.prepareStatement(sql);
 			
+			conn = this.dbUtil.getConnection();
+			String sql = "SELECT ebook_title ebookTitle, ebook_price ebookPrice FROM ebook ORDER BY ebook_date DESC LIMIT ?, ?";
+			stmt = conn.prepareStatement(sql);
 			stmt.setInt(1, beginRow);
 			stmt.setInt(2, rowPerPage);
-			
 			rs = stmt.executeQuery();
-			
-			while(rs.next());
+			while(rs.next()) {
 				Ebook ebook = new Ebook();
 				ebook.setEbookTitle(rs.getString("ebookTitle"));
 				ebook.setEbookPrice(rs.getInt("ebookPrice"));
-				ebook.setEbookImg(rs.getString("ebookImg"));
+				list.add(ebook);
+				
+			}
 			
 		} catch(Exception e) {
 			e.printStackTrace();
 			
 		} finally {
-			this.dbutil.close(rs, stmt, conn);
+			
+			this.dbUtil.close(rs, stmt, conn);
 			
 		}
 		return list;
 	}
-
 }
+ 
